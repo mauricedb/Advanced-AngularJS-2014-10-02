cad75fc0a5c34c02d8302488fb469564b873105c
Maurice de Beijer <mauricedb@computer.org>
2014-10-02 16:41:04 -0400

    Module 3 - AngularJS Directives

---
+++ Module 3/Public/App/App.js
@@ -0,0 +1,32 @@
+﻿(function () {
+
+    var app = angular.module("demoApp", []);
+
+    app.directive("setFocus", function () {
+        return {
+            link: function (scope, element) {
+                element[0].focus();
+
+                element.bind("click", function () { });
+            }
+
+        }
+
+    });
+
+    app.directive("newContent", function () {
+
+        return {
+            //template: "<div>This is the template</div>"
+            templateUrl: "templates/content.html",
+            link: function (scope) {
+                scope.showMessage = function (msg) {
+                    alert(msg)
+                }
+            },
+            transclude: true,
+            scope: true
+        };
+    });
+
+}());
