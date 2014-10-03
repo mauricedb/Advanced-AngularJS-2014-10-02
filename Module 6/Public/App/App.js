1dc57999db249e57cb43dd596e3e5dbd1944167c
Maurice de Beijer <mauricedb@computer.org>
2014-10-03 16:46:31 -0400

    Module 6 - AngularJS Internals

---
+++ Module 6/Public/App/App.js
@@ -0,0 +1,10 @@
+﻿(function () {
+
+    var app = angular.module("demoApp", ["ngSanitize"]);
+
+    app.controller("demoCtrl", function ($scope, $sce) {
+        $scope.snippet = "<span class='red' onclick='alert()'>Hello</span>";
+        $scope.safeSnippet = $sce.trustAsHtml($scope.snippet);
+    });
+
+}());
---
+++ Module 6/Public/App/Demo.js
@@ -0,0 +1,41 @@
+﻿var watches = [];
+function apply() {
+    watches.forEach((function (watch) {
+        var value = watch.watcher();
+        if (value !== watch.previous) {
+            watch.previous = value;
+            watch.notify(value);
+        }
+    }));
+}
+
+var data = {
+    name: ""
+};
+
+//Object.observe(data, function(e) {
+//    console.log(e);
+//});
+
+
+
+watches.push({
+    watcher: function() {
+        return data.name;
+    },
+    notify:function(value) {
+        console.log("New value:", value)
+    }
+});
+
+apply()
+data.name = "Maurice";
+apply()
+
+
+
+data.name = "John";
+apply()
+
+data.x = 1;
+apply()
